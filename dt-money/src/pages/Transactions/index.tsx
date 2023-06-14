373cc2fe07dc1ee5a620139b29a1a892f574f6a8
robsu17 <robson.costa134@gmail.com>
2023-06-14 20:46:16 -0300

    Contexts criado

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,31 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
 import { Searchform } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-interface Transaction {
-  id: number,
-  description: string,
-  type: 'income' | 'outcome',
-  price: number,
-  category: string,
-  createdAt: string
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions () {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-  
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
